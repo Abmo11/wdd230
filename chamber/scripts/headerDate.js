84b7e61204312bdcf80de2f949fcc979be745cf1
Abmo11 <mor18053@byui.edu>
2023-05-19 13:38:32 -0500

    index content 1st

--- chamber/scripts/headerDate.js
+++ chamber/scripts/headerDate.js
@@ -3,10 +3,15 @@ const datefield = document.querySelector("#headerDate");
 
 // derive the current date using a date object
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-  now
-);
-
-// "full" or long, medium, short options ... try them
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 
 datefield.innerHTML = `${fulldate}`;
+
+
+//Banner for Tuesdays
+
+const today = now.getDay();
+
+if (today === 1 || today === 2) {
+  document.querySelector('#monTueBanner').style.display = "block";
+}
