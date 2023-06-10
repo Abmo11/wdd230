041ae586d3c3d5d2419d5e25161fc5c1fe02faa5
Abmo11 <mor18053@byui.edu>
2023-06-10 13:49:58 -0500

    Join 2

--- chamber/scripts/hidden-join-date.js
+++ chamber/scripts/hidden-join-date.js
@@ -1,7 +1,7 @@
 
 function addHiddenDate() {
     //Creates a new Date object and pulls the current year in full 
-    let newTime = new Date();
+    const newTime = new Date();
 
     //Goes to the document, gets the element with the "hiddenJoinDate" id, and inserts the value into the textarea
     document.getElementById("hiddenJoinDate").textContent = newTime;
